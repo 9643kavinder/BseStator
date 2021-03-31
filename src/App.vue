c7fb4cb13fe5482e19e92b2f2c7e2c774c2b1765
Kavinder Panwar <i-kavinder.panwar@round.glass>
2021-03-31 14:44:18 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form @submit.prevent = "searchcompany">
+      <input type="search" class="form-outline form-control" placeholder="Enter The Company Name"
+      aria-label="Search" v-model="name"/>
+    </form>
+    <table class="table mt-4">
+      <thead>
+        <th>Company Name</th>
+        <th>Code</th>
+        <th>Open</th>
+        <th>High</th>
+        <th>Low</th>
+        <th>Close</th>
+      </thead>
+      <tbody>
+        <tr v-for="company in companies" :key="company.name">
+          <td>{{company.name}}</td>
+          <td>{{company.code}}</td>
+          <td>{{company.open}}</td>
+          <td>{{company.high}}</td>
+          <td>{{company.low}}</td>
+          <td>{{company.close}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+          name : '',
+          companies : []
+    }
+  },
+  async created(){
+      var response = await fetch ('http://127.0.0.1:8000/api/');
+      this.companies = await response.json();
+  },
+  methods : {
+    async searchcompany(){
+      var response = await fetch (`http://127.0.0.1:8000/api/${this.name}`);
+      this.companies = await response.json();
+      // this.name = '';
+    }
   }
+
+
 }
+
 </script>
 
 <style>
@@ -25,4 +63,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
